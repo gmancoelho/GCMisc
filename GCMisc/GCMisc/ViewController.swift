6846bf6defd939471baa6a4cbd0863e3d299484d
Guilherme <guizin.coelho@gmail.com>
2018-08-10 15:44:38 -0300

    Init

---
+++ GCMisc/GCMisc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GCMisc
+//
+//  Created by Guilherme Coelho on 10/08/18.
+//  Copyright © 2018 GuilhermeCoelhoTW. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
